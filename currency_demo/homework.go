045fbc2eddc046ad81b8bc93d0c41268b25f80a2
Jun Ge <gej@deepblueai.com>
2021-05-09 21:51:01 +0800

    commit homework3

--- currency_demo/homework.go
+++ currency_demo/homework.go
@@ -12,84 +12,98 @@ import (
 	"syscall"
 )
 
-
-
 /**
   目标：基于 errgroup 实现一个 http server 的启动和关闭，
   以及 linux signal 信号的注册和处理，要保证能够一个退出，全部注销退出。
 */
 
 type HttpServer struct {
-	name string
-	addr string
-	ctx context.Context
-	server http.Server
-	shutdown func()
+	server *http.Server
+	name   string
+	addr   string
+	ctx    context.Context
 }
-
 
 func (s *HttpServer) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	path := req.URL.Path
 	if path == "/shutdown" {
-		fmt.Fprintf(w, "shutdown server[%s] success\n", s.name)
+		n, err := fmt.Fprintf(w, "shutdown server[%s] success\n", s.name)
+		log.Printf("n: %d, err: %v \n", n, err)
 		log.Printf("app[%s]被人为关闭\n", s.name)
 		// 模拟APP异常退出，检测其它app是否会同步注销退出
-		s.shutdown()
+		_ = s.Stop()
 	} else {
-		fmt.Fprintf(w,"server[%s], is serving on addr[%s], current URL.Path = %q\n", s.name, s.addr, path)
+		n, err := fmt.Fprintf(w, "server[%s], is serving on addr[%s], current URL.Path = %q\n", s.name, s.addr, path)
+		log.Printf("n: %d, err: %v \n", n, err)
 	}
+}
+
+func NewHttpServer(ctx context.Context, name string, addr string) *HttpServer {
+	srv := &HttpServer{
+		name: name,
+		addr: addr,
+		ctx:  ctx,
+	}
+	srv.server = &http.Server{Handler: srv, Addr: addr}
+	return srv
 
 }
 
-func startHttpServer(ctx context.Context, name string, addr string) error {
+func (s *HttpServer) Run() error {
 	defer func() {
-		log.Printf("优雅退出server【%s】\n", name)
+		log.Printf("优雅退出server【%s】\n", s.name)
 	}()
-
-	app := &HttpServer{name: name, addr: addr}
-
-	srv := http.Server{
-		Addr:    addr,
-		Handler: app,
-	}
-
-	app.shutdown = func() {
-		srv.Shutdown(ctx)
-	}
 
 	stop := make(chan error)
-	go func(){
-		stop <- srv.ListenAndServe()
+	go func() {
+		log.Printf("Server[%s] running\n", s.name)
+		stop <- s.server.ListenAndServe()
 	}()
+
+	select {
+	case <-s.ctx.Done():
+		log.Printf("app[%s]接收到context退出通知，shutdown\n", s.name)
+		return s.Stop()
+	case stopErr := <-stop:
+		return errors.Wrapf(stopErr, "app[%s]异常终止", s.name)
+	}
+}
+
+func (s *HttpServer) Stop() error {
+
+	log.Printf("server[%s] stopping\n", s.name)
+	return s.server.Shutdown(s.ctx)
+}
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	g, ctx := errgroup.WithContext(ctx)
+
+	server1 := NewHttpServer(ctx, "server1", ":8001")
+	server2 := NewHttpServer(ctx, "server2", ":8002")
+
+	g.Go(func() error {
+		return server1.Run()
+	})
+	g.Go(func() error {
+		return server2.Run()
+	})
 
 	exit := make(chan os.Signal)
 	signal.Notify(exit, syscall.SIGINT, syscall.SIGTERM)
 
-	select {
-	case <-ctx.Done():
-		log.Printf("app[%s]接收到context退出通知，shutdown\n", app.name)
-		return srv.Shutdown(ctx)
-	case <-exit:
-		log.Printf("app[%s]接收到系统退出信号，shutdown\n", app.name)
-		return srv.Shutdown(ctx)
-	case stopErr := <- stop:
-		return errors.Wrapf(stopErr, "app[%s]异常终止", app.name)
-	}
-}
-
-
-func main() {
-
-	ctx := context.Background()
-	g, ctx:= errgroup.WithContext(ctx)
-
 	g.Go(func() error {
-		return startHttpServer(ctx,"app1", ":8001")
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case <-exit:
+			cancel()
+			return nil
+		}
 	})
-	g.Go(func() error {
-		return startHttpServer(ctx,"app2", ":8002")
-	})
-	log.Printf("程序已运行，当前进程pid：%d \n" ,os.Getpid())
+
+	log.Printf("程序已运行，当前进程pid：%d \n", os.Getpid())
+
 	if err := g.Wait(); err != nil {
 		log.Printf("find app run error: %v\n", err.Error())
 		log.Printf("close all apps and exit.")
